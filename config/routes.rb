e29666c87c9c366d0aec5b58bed780412001a5e5
belammuia <belammuia0@gmail.com>
2023-12-15 12:56:24 +0300

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :students
+  resources :students, only: [:create,:show]
+  post '/register', to: 'students#create'
+  get '/me/:id', to: 'students#show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
