2ade7f6d0f9eae781bb7f37887fe239dbe3e46ae
belammuia <belammuia0@gmail.com>
2023-12-19 18:30:33 +0300

    changes

--- app/controllers/admins/sessions_controller.rb
+++ app/controllers/admins/sessions_controller.rb
@@ -1,19 +1,20 @@
 class Admins::SessionsController < ApplicationController
-    def create
-      admin = Admin.find_by(email: params[:email])
-  
-      if admin && admin.authenticate(params[:password])
-        session[:admin_id] = admin.id
-        render json: admin, status: :accepted
-      else
-        render json: { error: 'Invalid Admin credentials' }, status: :unauthorized
-      end
-    end
- 
-    def destroy
-        session[:admin_id] = nil
-        render json: { message: 'Admin logged out successfully' }, status: :ok
-      end
+  def create
 
+
+    admin = Admin.find_by(email: params[:email])
+
+    if admin && admin.authenticate(params[:password])
+      session[:admin_id] = admin.id
+      render json: admin, status: :accepted
+    else
+ 
+      render json: { error: 'Invalid Admin credentials' }, status: :unauthorized
+    end
   end
-  
+
+  def destroy
+    session[:admin_id] = nil
+    render json: { message: 'Admin logged out successfully' }, status: :ok
+  end
+end
