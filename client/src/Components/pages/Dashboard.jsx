b0bbc41efa66a71bfc4c2827e2a70d3058ae6327
belammuia <belammuia0@gmail.com>
2023-12-20 18:24:02 +0300

    :bug: Fix minor bugs

--- client/src/Components/pages/Dashboard.jsx
+++ client/src/Components/pages/Dashboard.jsx
@@ -1,18 +1,42 @@
 import React from 'react';
-import { Outlet, Routes, Route } from 'react-router-dom';
+import { Outlet, Routes, Route, Navigate } from 'react-router-dom';
 import Logout from './Logout';
 import Layout from './Layout';
-function Dashboard() {
+import Grades from './Grades';
+
+function Dashboard({isLoggedIn}) {
+  console.log('Dashboard rendered')
+
+  if(!isLoggedIn){
+    <Navigate to='/' />
+  }
   return (
-    <div>
-   
- <Layout />
-      <Outlet />
+  
+   <>
+   <Layout>
       <Routes>
-        <Route path="logout" element={<Logout />} />
+        <Route
+          path="/"
+          element={
+            <div className="dashboard-content flex-grow p-6">
+              <h1>Welcome to the Dashboard</h1>
+              <Outlet />
+            </div>
+          }
+        >
+          <Route index element={<Grades />} />
+          {/* <Route path="student-info" element={<StudentInfo />} />
+          <Route path="voucher" element={<Voucher />} />
+          <Route path="grades" element={<Grades />} />
+          <Route path="fees" element={<Fees />} />
+          <Route path="teachers" element={<Teachers />} />
+          <Route path="settings" element={<Settings />} /> */}
+   
+        </Route>
       </Routes>
-    </div>
-  );
+    </Layout>
+    </>
+  )
 }
 
 export default Dashboard;
--- client/src/Components/pages/Layout.jsx
+++ client/src/Components/pages/Layout.jsx
@@ -3,16 +3,22 @@
 import React from 'react';
 import Nav from './Nav';
 import Sidebar from './Sidebar';
-import { Outlet } from 'react-router-dom';
-
+import { Outlet, Routes, Route } from 'react-router-dom';
+import Logout from './Logout';
 const Layout = () => {
   return (
     <div>
       <Nav />
       <div className="flex">
         <Sidebar />
-        <div className="flex-grow p-6">
+        <div className=" w-full"> 
+        <Routes>
+            <Route path="logout" element={<Logout />} />
+        
+            {/* Add other routes as needed */}
+          </Routes>
           <Outlet />
+
         </div>
       </div>
     </div>
