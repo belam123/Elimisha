276e65974aa52b003a819996c6e2eade7194abde
belammuia <belammuia0@gmail.com>
2023-12-21 22:54:40 +0300

    :ambulance: Fix styling

--- client/src/Components/pages/Sidebar.jsx
+++ client/src/Components/pages/Sidebar.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Link, Outlet, Routes, Route } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { AiOutlineBook, AiOutlineUser, AiOutlineDollar, AiOutlineAmazon, AiOutlineLogout, AiOutlineTeam, AiOutlineSetting } from 'react-icons/ai';
 import Logout from './Logout';
 
-function Sidebar({onLogout}) {
+function Sidebar({ onLogout }) {
   return (
-    <div className="bg-gray h-screen flex">
-      <div className="bg-white shadow-lg w-64">
-        <div className="flex-col pt-5 flex overflow-y-auto">
-          <div className="h-full flex-col justify-between px-4 flex">
+    <div className="flex h-screen bg-gray">
+      <div className="w-64 bg-white shadow-lg">
+        <div className="flex flex-col h-full overflow-y-auto pt-5">
+          <div className="flex flex-col justify-between h-full px-4">
             <div className="space-y-4">
               <LinkButton icon={<AiOutlineUser />} text="Student Information" to="/dashboard/student-info" />
               <LinkButton icon={<AiOutlineAmazon />} text="Voucher" to="/dashboard/voucher" />
@@ -16,22 +16,18 @@ function Sidebar({onLogout}) {
               <LinkButton icon={<AiOutlineDollar />} text="Fees" to="/dashboard/fees" />
               <LinkButton icon={<AiOutlineTeam />} text="Teachers" to="/dashboard/teachers" />
               <LinkButton icon={<AiOutlineSetting />} text="Settings" to="/dashboard/settings" />
-             
             </div>
             <Logout onLogout={onLogout} />
           </div>
         </div>
       </div>
-
     </div>
   );
 }
 
 const LinkButton = ({ icon, text, to }) => (
-  <Link to={to} className="font-medium text-sm items-center rounded-lg text-gray-900 px-4 py-2.5 flex transition-all duration-200 hover:bg-gray-200 group cursor-pointer w-full text-left">
-    <span className="justify-center items-center flex">
-      {icon}
-    </span>
+  <Link to={to} className="flex items-center w-full px-4 py-2.5 text-sm font-medium text-gray-900 transition-all duration-200 hover:bg-gray-200 group cursor-pointer">
+    <span className="flex items-center justify-center">{icon}</span>
     <span>{text}</span>
   </Link>
 );
