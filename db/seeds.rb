acceb49cdb81705ec3041d3a3e9a18d543caba2c
belammuia <belammuia0@gmail.com>
2024-01-02 23:06:20 +0300

    changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,14 +5,12 @@ admin1_password = ENV['ADMIN1_PASSWORD'] || 'default_password'
 admin2_password = ENV['ADMIN2_PASSWORD'] || 'default_password'
 
 stud1_password = ENV['STUD1_PASSWORD'] || 'default_password'
-stud2_password = ENV['STUD2_PASSWORD'] || 'default_password'
-stud3_password = ENV['STUD3_PASSWORD'] || 'default_password'
+
 
 hashed_password1 = BCrypt::Password.create(admin1_password)
 hashed_password2 = BCrypt::Password.create(admin2_password)
 hashed_password3 = BCrypt::Password.create(stud1_password)
-hashed_password4 = BCrypt::Password.create(stud2_password)
-hashed_password5 = BCrypt::Password.create(stud3_password)
+
 
 
 puts 'seeding...'
@@ -28,11 +26,19 @@ admin1 = Admin.create(email: "belammuia0@gmail.com", password: hashed_password1)
 admin2 = Admin.create(email: "clivemudigo@gmail.com", password: hashed_password2)
 admin3 = Admin.create!(email: 'muia@gmail.com', password: '12345').generate_access_token
 
-#students
-student1 =  Student.create(first_name: "James", second_name: "Kamau", last_name: "Mashu", email: "kmashu@kibwareboys.ac.ke",password: hashed_password3,password_confirmation: hashed_password3, form_id: form1.id)
-student2 =  Student.create(first_name: "Brian", second_name: "Muchiri", last_name: "Nderitu",email: "bmuchiri@kibwareboys.ac.ke", password: hashed_password4, password_confirmation: hashed_password4,form_id: form4.id)
-student3 = Student.create(first_name: "Joshua", second_name: "Ochieng", last_name: "Aduor", email: "joduor@kibwareboys.ac.ke", password: hashed_password5, password_confirmation: hashed_password5, form_id: form2.id)
 
+student1 = Student.create(
+    first_name: "Belam",
+    second_name: "Muia",
+    last_name: "Nzioka",
+    email: "bmuia@careercampus.ac.ke",
+    password: 'belam',
+    password_confirmation: 'belam',
+    form_id: form4.id
+  )
+  
+
+  
 #teachers
 teacher1 = Teacher.create(first_name: "Paul", last_name: "Wainaina", email: "balozi@gmail.com",department: "Soc1al sciences and humanities")
 teacher2 = Teacher.create(first_name: "John", last_name: "Koli", email: "kolingo@gmail.com",department: "Social sciences and humanities")
@@ -43,13 +49,9 @@ teacher6 = Teacher.create(first_name: "Emilia", last_name: "Makena", email: "mak
 
 #vouchers
 voucher1 = Voucher.create(voucher_amount: 1050, expiry_date: "19-04-2023", student_id: student1.id)
-voucher2 = Voucher.create(voucher_amount: 2250, expiry_date: "27-05-2023", student_id: student3.id)
-voucher3 = Voucher.create(voucher_amount: 10500, expiry_date: '20-11-2024', student_id: student2.id) 
 
 #fees
 fee1 =  Fee.create(amount: 1500, due_date: '04-04-2023', payment_status: "Not cleared", student_id: student1.id)
-fee2 =  Fee.create(amount: 0, due_date: 'NA', payment_status: "Cleared", student_id: student3.id)
-fee3 = Fee.create(amount: 0, due_date: 'NA', payment_status: 'Cleared', student_id: student2.id)
 
 
 
@@ -65,7 +67,18 @@ subject7 = Subject.create(name:'music',teacher_id: teacher1.id)
 subject8 = Subject.create(name:'french',teacher_id: teacher2.id)
 subject9 = Subject.create(name:'arts', teacher_id: teacher3.id)
 subject10 = Subject.create(name:'drama',teacher_id: teacher4.id)
-
+#marks 
+mark1 = Mark.create(
+    exam_score: 70,
+    assignment_score: 65,
+    quiz_score: 90,
+    subject_id: subject1.id,
+    student_id: student1.id
+)
+mark2 = Mark.create(exam_score: 80, assignment_score: 79, quiz_score: 45, subject_id: subject2.id, student_id: student1.id);
+mark3 = Mark.create(exam_score: 40, assignment_score: 30, quiz_score: 75, subject_id: subject3.id, student_id: student1.id);
+mark4 = Mark.create(exam_score: 90, assignment_score: 69, quiz_score: 55, subject_id: subject4.id, student_id: student1.id);
+mark5 = Mark.create(exam_score: 61, assignment_score: 72, quiz_score: 45, subject_id: subject5.id, student_id: student1.id);
 #add more seeding
 
 
