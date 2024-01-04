b868c4dfa74101286750d1862229e0d25a4dfd7d
belammuia <belammuia0@gmail.com>
2024-01-04 18:27:37 +0300

    changes

--- config/application.rb
+++ config/application.rb
@@ -1,12 +1,12 @@
 require_relative "boot"
-require 'dotenv'
+
 
 require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
+
 
 
 module Elimisha
